46acc54bd354395704f7c5d5014a7af400852491
crabsatellite <islicy@outlook.com>
2023-06-06 16:06:09 +0800

    Initial commit

---
+++ app/src/main/java/com/crab/crabmelodies/ui/theme/Theme.kt
@@ -0,0 +1,26 @@
+package com.crab.crabmelodies.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+//customized update
+private val DarkColorPalette = darkColors(
+    background = Color.Black,
+    surface =  Color.Black
+)
+
+@Composable
+fun SpotifyTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    //customized update
+    val colors = DarkColorPalette
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
