874da118398005664d700211f5edd402cdbc9362
crabsatellite <islicy@outlook.com>
2023-06-07 11:38:03 +0800

    update LoadingSection

--- app/src/main/java/com/crab/crabmelodies/MainActivity.kt
+++ app/src/main/java/com/crab/crabmelodies/MainActivity.kt
@@ -3,24 +3,17 @@ package com.crab.crabmelodies
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.crab.crabmelodies.ui.theme.SpotifyTheme
 
 // customized extend AppCompatActivity
@@ -34,24 +27,9 @@ class MainActivity : AppCompatActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LoadingSection("Screen is loading")
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true, widthDp = 412, heightDp = 732)
-@Composable
-fun DefaultPreview() {
-    SpotifyTheme {
-        Surface {
-            AlbumCover()
         }
     }
 }
@@ -67,30 +45,12 @@ private fun LoadingSection(text: String) {
     }
 }
 
+@Preview(showBackground = true, widthDp = 412, heightDp = 732)
 @Composable
-private fun AlbumCover() {
-    Column {
-        Box(modifier = Modifier.size(160.dp)) {
-            AsyncImage(
-                model = "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg",
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.FillBounds
-            )
-            Text(
-                text = "Still Fantasy",
-                color = Color.White,
-                modifier = Modifier
-                    .padding(bottom = 4.dp, start = 2.dp)
-                    .align(Alignment.BottomStart),
-            )
+fun DefaultPreview() {
+    SpotifyTheme {
+        Surface {
+            LoadingSection("Screen is loading") // <- this one
         }
-
-        Text(
-            text = "Jay Chou",
-            modifier = Modifier.padding(top = 4.dp),
-            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
-            color = Color.LightGray,
-        )
     }
 }
