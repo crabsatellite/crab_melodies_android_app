d36f9a55aadb78303a27bd7fbe074715d2eff26e
crabsatellite <islicy@outlook.com>
2023-06-08 13:53:41 +0800

    update HomeScreen

--- app/src/main/java/com/crab/crabmelodies/ui/theme/home/HomeFragment.kt
+++ app/src/main/java/com/crab/crabmelodies/ui/theme/home/HomeFragment.kt
@@ -1,13 +1,13 @@
 package com.crab.crabmelodies.ui.theme.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 
--- app/src/main/java/com/crab/crabmelodies/ui/theme/home/HomeScreen.java
+++
@@ -1,7 +0,0 @@
-package com.crab.crabmelodies.ui.theme.home;
-
-import androidx.compose.runtime.Composable;
-
-@Composable
-public class HomeScreen {
-}
