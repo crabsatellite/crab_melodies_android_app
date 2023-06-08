349cdc6aa62a08b780996e2dee3d42986df30ab6
crabsatellite <islicy@outlook.com>
2023-06-08 13:12:23 +0800

    fetch the HomeUistate in the view layer

--- app/src/main/java/com/crab/crabmelodies/ui/theme/home/HomeFragment.kt
+++ app/src/main/java/com/crab/crabmelodies/ui/theme/home/HomeFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.crab.crabmelodies.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,5 +20,11 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-}
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (viewModel.uiState.value.isLoading) {
+            viewModel.fetchHomeScreen()
+        }
+    }
+}
