a8305f658554739fba403cbb22ba959f966c90d8
crabsatellite <islicy@outlook.com>
2023-06-08 12:20:32 +0800

    add @AndroidEntrypoint annotation

--- app/src/main/java/com/crab/crabmelodies/ui/theme/home/HomeFragment.kt
+++ app/src/main/java/com/crab/crabmelodies/ui/theme/home/HomeFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.crab.crabmelodies.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
