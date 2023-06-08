4fedb24c2988b14c730c72aa64621de228fe4fbb
crabsatellite <islicy@outlook.com>
2023-06-08 10:33:23 +0800

    add album data

---
+++ app/src/main/java/com/crab/crabmelodies/datamodel/Album.kt
@@ -0,0 +1,28 @@
+package com.crab.crabmelodies.datamodel
+
+import com.google.gson.annotations.SerializedName
+
+data class Album(
+    val id: Int,
+    @SerializedName("album")
+    val name: String,
+    val year: String,
+    val cover: String,
+    val artists: String,
+    val description: String
+) {
+
+    companion object {
+        fun empty(): Album {
+            return Album(
+                id = -1,
+                "",
+                "",
+                "",
+                "",
+                "",
+            )
+        }
+    }
+}
+
