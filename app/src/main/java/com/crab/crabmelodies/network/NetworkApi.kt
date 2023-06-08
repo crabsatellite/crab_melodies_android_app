296cad8070873583bf5c5a1296647954e12a3f65
crabsatellite <islicy@outlook.com>
2023-06-08 15:42:48 +0800

    update playlist api to gain playlist

--- app/src/main/java/com/crab/crabmelodies/network/NetworkApi.kt
+++ app/src/main/java/com/crab/crabmelodies/network/NetworkApi.kt
@@ -1,10 +1,16 @@
 package com.crab.crabmelodies.network
 
 import Section
+import com.crab.crabmelodies.datamodel.Playlist
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface NetworkApi {
     @GET("feed")
     fun getHomeFeed(): Call<List<Section>>
+
+    @GET("playlist/{id}")
+    fun getPlaylist(@Path("id") id: Int): Call<Playlist>
+
 }
