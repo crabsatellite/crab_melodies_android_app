0e9f6efc79465a50b53a491083e1c3b44c543e02
crabsatellite <islicy@outlook.com>
2023-06-08 13:36:01 +0800

    add delay in HomeRepository

--- app/src/main/java/com/crab/crabmelodies/repository/HomeRepository.kt
+++ app/src/main/java/com/crab/crabmelodies/repository/HomeRepository.kt
@@ -3,12 +3,14 @@ package com.crab.crabmelodies.repository
 import Section
 import com.crab.crabmelodies.network.NetworkApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class HomeRepository @Inject constructor(private val networkApi: NetworkApi) {
 
     suspend fun getHomeSections(): List<Section> = withContext(Dispatchers.IO) {
+        delay(3000)
         networkApi.getHomeFeed().execute().body()!!
     }
 }
