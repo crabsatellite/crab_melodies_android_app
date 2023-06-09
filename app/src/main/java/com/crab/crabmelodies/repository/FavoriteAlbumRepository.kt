39ed11c8435d5104b22317899b364a20d66da78e
crabsatellite <islicy@outlook.com>
2023-06-09 12:00:07 +0800

    add FavoriteAlbumRepository

---
+++ app/src/main/java/com/crab/crabmelodies/repository/FavoriteAlbumRepository.kt
@@ -0,0 +1,23 @@
+package com.crab.crabmelodies.repository
+
+import com.crab.crabmelodies.database.DatabaseDao
+import com.crab.crabmelodies.datamodel.Album
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class FavoriteAlbumRepository @Inject constructor(private val databaseDao: DatabaseDao) {
+
+    fun isFavoriteAlbum(id: Int): Flow<Boolean> =
+        databaseDao.isFavoriteAlbum(id).flowOn(Dispatchers.IO)
+
+    suspend fun favoriteAlbum(album: Album) = withContext(Dispatchers.IO) {
+        databaseDao.favoriteAlbum(album)
+    }
+
+    suspend fun unFavoriteAlbum(album: Album) = withContext(Dispatchers.IO) {
+        databaseDao.unFavoriteAlbum(album)
+    }
+}
